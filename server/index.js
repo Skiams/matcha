8006074bd97e07f34ed084d3a07fc3eba6b8fee6
Elsa TOUMA <eltouma@f1r8s18.clusters.42paris.fr>
2025-11-12 20:08:24 +0100

    [EDIT USER] Edit username

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ const corsOptions = {
 const { Client } = pg;
 
 const client = new Client({
-    user: 'USER',
+    user: 'USER_TEST',
     host: 'db',
     database: 'postgres',
     password: 'te6%hed!^8*',
